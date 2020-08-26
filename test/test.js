c8d419b6b8f08c95c03a4480c8e2e7cd02608101
hackyminer <hackyminer@gmail.com>
2020-08-27 03:30:32 +0900

    update test.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var Buffer = require('buffer').Buffer;
-var cu = require('../cuda');
+var cu = require('bindings')('cuda');
 
 //cuDriverGetVersion
 //cuDeviceGetCount
@@ -7,7 +7,7 @@ console.log("Node-cuda exports:", cu);
 
 for (var i = 0; i < cu.deviceCount; i++) {
     //cuDeviceGet
-    var cuDevice = new cu.Device(i);
+    var cuDevice = cu.deviceGet(i);
 
     //cuDeviceComputeCapability
     //cuDeviceGetName
@@ -16,7 +16,7 @@ for (var i = 0; i < cu.deviceCount; i++) {
 }
 
 //cuCtxCreate
-var cuCtx = new cu.Ctx(0, cu.Device(0));
+var cuCtx = cu.ctxCreate(0, cu.deviceGet(0));
 
 //cuCtxGetApiVersion
 console.log("Created context:", cuCtx);
@@ -26,7 +26,7 @@ var cuMem = cu.memAllocPitch(100, 100, 8);
 console.log("Allocated 100x100 array of doubles:", cuMem);
 
 //cuMemFree
-var error = cuMem.free();
+var error = cu.memFree(cuMem);
 console.log("Mem Free with error code: " + error);
 
 //cuMemAlloc
@@ -40,7 +40,7 @@ for (var i = 0; i < buf.length; i++) {
 console.log("Created buffer of 100 bytes:", buf);
 
 // cuMemcpyHtoD
-var error = cuMem.copyHtoD(buf);
+var error = cu.memcpyHtoD(cuMem, buf);
 console.log("Copied buffer to device:", error);
 
 //cuModuleLoad
@@ -48,21 +48,20 @@ var cuModule = cu.moduleLoad("test/test.cubin");
 console.log("Loaded module:", cuModule);
 
 //cuModuleGetFunction
-var cuFunction = cuModule.getFunction("helloWorld");
+var cuFunction = cu.moduleGetFunction(cuModule, "helloWorld");
 console.log("Got function:", cuFunction);
 
-//cuLaunchKernel
-var error = cu.launch(cuFunction, [3, 1, 1], [2, 2, 2],
-[{
-    type: "DevicePtr",
-    value: cuMem.devicePtr
-}]);
+var error = cu.launchKernel(cuFunction, [3, 1, 1], [2, 2, 2], cuMem);
 console.log("Launched kernel:", error);
 
 // cuMemcpyDtoH
-var error = cuMem.copyDtoH(buf, true);
+var error = cu.memcpyDtoH(cuMem, buf, true);
 console.log("Copied buffer to host:", error);
 
+var error = cu.ctxSynchronize(cuCtx);
+
+console.log("Context destroyed with error code: " + error);
+/*
 //cuCtxSynchronize
 var error = cuCtx.synchronize(function(error) {
     console.log("Context synchronize with error code: " + error);
@@ -75,3 +74,5 @@ var error = cuCtx.synchronize(function(error) {
     error = cuCtx.destroy();
     console.log("Context destroyed with error code: " + error);
 });
+
+*/
